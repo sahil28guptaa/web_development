1da49d9b628d6c356b0960704296f4d25f4a9582
sahil28guptaa <sahil28gupta28@gmail.com>
2024-08-13 15:35:34 +0530

    complete project

---
+++ file1.js
@@ -0,0 +1,81 @@
+// // Save file with name file.js
+// //document.write("CALC");
+// var x = 120;
+// var y = 30;
+// document.write("Addition=",x+y,"<br>")
+// document.write("Subtraction=",x-y,"<br>")
+// document.write("Multiplication=",x*y,"<br>")
+// document.write("Devide=",x/y,"<br>")
+// -------------------------------------
+// console.log("hello, this is cansole");
+// -----------------
+// var x = 120;
+// function xyz(){
+//     var y = 30;
+// }
+// console.log(x,y);
+//const x = 12;
+//{
+//    const y = 30
+    
+//}
+//console.log(x,y);
+// let a = 10;
+// document.write(a,"<br>")
+// let b = 5;
+// document.write(b,"<br>")
+// let sum = a + b;
+// document.write(sum,"<br>");
+// let difference = a - b;
+// document.write(difference,"<br>");
+// let product = a * b;
+// document.write(product,"<br>");
+// let quotient = a / b;
+// document.write(quotient,"<br>");
+// let remainder = a % b;
+// document.write(remainder,"<br>");
+// let isequal = a == b;
+// document.write(isequal,"<br>");
+// let isnotequal = a != b;
+// document.write(isnotequal,"<br>");
+// let isgreater = a > b;
+// document.write(isgreater,"<br>");
+// let logicaland = (a > b && b <10);
+// document.write(logicaland,"<br>");
+// 
+// simple function
+ function greeting(){
+     document.getElementById("demo").innerHTML = "Good Morning"
+     document.getElementById("mode").style.background = "skyblue"
+     document.getElementById("mode").style.color = "black"
+ }
+ function a(){
+    document.getElementById("demo").innerHTML = "Good Afternoon"
+    document.getElementById("mode").style.background = "yellow"
+     document.getElementById("mode").style.color = "black"
+}
+function b(){
+    document.getElementById("demo").innerHTML = "Good Evening"
+    document.getElementById("mode").style.background = "green"
+     document.getElementById("mode").style.color = "black"
+}
+function c(){
+    document.getElementById("demo").innerHTML = "Good Night"
+    document.getElementById("mode").style.background = "black"
+     document.getElementById("mode").style.color = "white"
+}                                                                     
+// //greeting();
+// // function Expression
+// let day = function(){
+//     document.write("Have a nice day<br>")
+// }
+// //day()
+// //arrow function
+// let msg = () => {
+//     document.write("Happy Birthday<br>")
+//}
+//msg()
+//iife immediately invoked function expression
+// (function(){
+//     document.write("this is IIFE function<br>")
+//     }());
